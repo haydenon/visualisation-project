be4d5f6a606abdee41343f96f359e79e0131d617
Hayden <hayden.oneill@paradise.net.nz>
2014-05-30 18:28:27 +1200

    added rivalry view

--- visualisation/index.js
+++ visualisation/index.js
@@ -1,15 +1,18 @@
-var w = window.innerWidth-40;
-var h = 550;
+var wd = window.innerWidth-40;
+var h = 500;
 var svg = d3.select("body")
 	.append("svg")
-	.attr("width", w)
+	.attr("width", wd)
 	.attr("height", h);
 window.onresize = function(event) {
-    w = window.innerWidth-40;
-    svg.attr("width",w)
+    wd = window.innerWidth-40;
+    svg.attr("width",wd)
     change();
 };
 var aniDur = 800;
+
+mode = d3.select("body").select("select#mode");
+mode.on("change",change);
 
 countrySort = d3.select("body").select("input#byCountry");
 countrySort.on("change",change);
@@ -19,6 +22,9 @@ accumulative.on("change",change);
 
 yearsRange = d3.select("body").select("input#years");
 yearsRange.on("change",change);
+
+roundsRange = d3.select("body").select("input#round");
+roundsRange.on("change",change);
 
 net = d3.select("body").select("input#net");
 net.on("change",change);
@@ -33,6 +39,8 @@ wins.on("change",change);
 goalsTog = d3.select("body").select("input#goals");
 goalsTog.on("change",change);
 
+chooser = d3.select("body").select("select#chooser");
+chooser.on("change",change)
 
 var aus = {};
 aus.names = "Australia"
@@ -86,7 +94,7 @@ d3.csv("data/final.csv", function(d) {
 function setupData(d){
 	var netballData = {};
 	netballData.venues = getVenues(d);
-	netballData.matches = getMatches(d, netballData.venues);
+	netballData.matches = getMatches(d, netballData.venues.venues);
 	//NEED MAP FROM TEAM TO RESULTS. (IS THIS EVERY SEASON? HOW DO WE DO THAT?)
 	//netballData.teamsToResults = getTeamToResults(d);
 	//NEED MAP FROM TEAM TO RIVALS
@@ -95,16 +103,23 @@ function setupData(d){
 }
 
 function getVenues(d){
+	venueList = [];
 	venues = {};
 	d.forEach(function(match){
-		if(venues[match.Venue]!=undefined){
-			venues[match.Venue] = {
-				name: match.Venue,
+		if(venues[match.Venue]==undefined){
+			var v = {
+				names: match.Venue,
 				matches: []
 			};
+			venues[match.Venue] = v;
+			venueList.push(v);
 		}
 	});
-	return venues;
+	venueList.sort(function(a,b){return a.names.localeCompare(b.names)});
+	return {
+		venues: venues,
+		venueList: venueList
+	};
 }
 
 function getMatches(d, venues){
@@ -126,6 +141,7 @@ function getMatches(d, venues){
 		var homeTeam =  teams.teamMap[match['Home Team']];
 		var awayTeam =  teams.teamMap[match['Away Team']];
 		var venue = venues[match.Venue];
+		venue.homeTeam = homeTeam;
 		var score = match.Score;
 		var draw = false;
 		var homeScore;
@@ -195,8 +211,28 @@ function winMatch(home){
 	};
 }
 
+var currMode;
+
 function change(){
-	
+	currMode = mode.property("value");
+	if(currMode=="Venues"){
+		var opts = chooser.selectAll("option")
+			.data(dataset.venues.venueList);
+		opts.enter()
+			.append("option")
+		opts.text(function(d){return d.names;})
+		opts.exit().remove();
+	}
+	else {
+		var tms = [{names: "All"}];
+		tms.push.apply(tms,teams.teams.slice(0))
+		var opts = chooser.selectAll("option")
+			.data(tms);
+		opts.enter()
+			.append("option");
+		opts.text(function(d){return d.names;})
+		opts.exit().remove();
+	}
 	redraw();
 }
 
@@ -220,13 +256,20 @@ function teamPosCompare(a,b){
 function getValues(){
 	var year = parseInt(yearsRange.property("value"))+2008;
 	var accum = accumulative.property('checked');
-	var round = 17
+	var round = parseInt(roundsRange.property("value"))
 	matches = dataset.matches.matches;
 	if(accum){
 		matches = matches.filter(matchesToYear(year,round));
 	}
 	else {
 		matches = matches.filter(matchesFromYear(year,round));
+	}
+	var venue;
+	if(currMode == "Venues"){
+		venue = dataset.venues.venues[chooser.property("value")];
+		matches = matches.filter(function(m){
+			return m.venue == venue;
+		})
 	}
 
 	var teamPlace = {};
@@ -295,13 +338,112 @@ function getValues(){
 	teamOrder = [];
 	teams.teams.forEach(function(t){
 		teamPlacement = teamPlace[t.names];
-	// 	teamPlacement.num = teamPlacement.totwins() - teamPlacement.totloses();
-	// 	teamPlacement.values = getPointWinValues(teamPlacement,false);
 		teamOrder.push(teamPlacement);
 	});
 	teamOrder.sort(teamPosCompare);
 	var teamData = getPointWinValues(teamOrder,teamPlace);
+	if(currMode=="Venues") teamData.homeTeam = venue.homeTeam;
 	return teamData;
+}
+
+function getRivalValues(rivalTeam){
+	var rt = teams.teamMap[rivalTeam];
+	var year = parseInt(yearsRange.property("value"))+2008;
+	var accum = accumulative.property('checked');
+	var round = parseInt(roundsRange.property("value"))
+	matches = dataset.matches.matches;
+	if(accum){
+		matches = matches.filter(matchesToYear(year,round));
+	}
+	else {
+		matches = matches.filter(matchesFromYear(year,round));
+	}
+	matches = matches.filter(function(m){
+		return m.homeTeam == rt || m.awayTeam == rt;
+	});
+
+	var tms = teams.teams.slice(0);
+	var ind = tms.indexOf(rt);
+	tms.splice(ind,1);
+
+	var teamPlace = {};
+	tms.forEach(function(t){
+		var obData = {
+			team: t,
+			points: 0,
+			homewins: 0, awaywins: 0, overwins: 0,
+			homeloses: 0, awayloses: 0, overloses: 0,
+			homefor: 0, homeag: 0,
+			awayfor: 0, awayag: 0,
+			overfor: 0, overag: 0,
+			place: 0
+		}
+		obData.totwins = function(){ return this.homewins+this.awaywins+this.overwins; }
+		obData.totloses = function(){ return this.homeloses+this.awayloses+this.overloses; }
+		obData.totfor = function(){ return this.homefor+this.awayfor+this.overfor; }
+		obData.totag = function(){ return this.homeag+this.awayag+this.overag; }
+		teamPlace[t.names] = obData;
+	});
+
+	matches.forEach(function(m){
+		isHome = rt == m.homeTeam;
+		over = m.homeTeam.country != m.awayTeam.country;
+
+		if(m.draw);
+		else {
+			if(m.homeWon){
+				if(isHome && over){teamPlace[m.awayTeam.names].overloses+=1;}
+				else if (isHome && !over){teamPlace[m.awayTeam.names].awayloses+=1;} 
+				else {
+					teamPlace[m.homeTeam.names].points+=2;
+					teamPlace[m.homeTeam.names].homewins+=1;
+				}
+			}
+			else {
+				if(isHome && over){
+					teamPlace[m.awayTeam.names].points+=2;
+					teamPlace[m.awayTeam.names].overwins+=1;
+				}
+				else if(isHome && !over){
+					teamPlace[m.awayTeam.names].points+=2;
+					teamPlace[m.awayTeam.names].awaywins+=1;
+				}
+				else {
+					teamPlace[m.homeTeam.names].homeloses+=1;
+				}
+			}
+		}
+		if(!isHome){
+			teamPlace[m.homeTeam.names].homefor+=m.homeScore;
+			teamPlace[m.homeTeam.names].homeag+=m.awayScore;
+		}
+		else{
+			if(over){
+				teamPlace[m.awayTeam.names].overfor+=m.awayScore;
+				teamPlace[m.awayTeam.names].overag+=m.homeScore;
+			}
+			else{
+				teamPlace[m.awayTeam.names].awayfor+=m.awayScore;
+				teamPlace[m.awayTeam.names].awayag+=m.homeScore;
+			}
+		}
+	});
+
+	teamOrder = [];
+	tms.forEach(function(t){
+		tP = teamPlace[t.names];
+		totgames = tP.totwins() + tP.totloses();
+		thres = totgames/4;
+		if(tP.totwins()<thres || tP.totloses()<thres) return;
+		thresdif = totgames*10;
+		if(Math.abs(tP.totfor() - tP.totag())>thresdif) return;
+		teamOrder.push(tP);
+	});
+	//console.log(teamOrder.length);
+	teamOrder.sort(teamPosCompare);
+	var teamData = getPointWinValues(teamOrder,teamPlace);
+	return teamData;
+
 }
 
 function getPointWinValues(to,tp){
@@ -366,9 +508,75 @@ function getPointWinValues(to,tp){
 }
 
 function redraw(){
-	teamData = getValues();
+	rivalTeam = chooser.property("value");
+	var teamData;
+	if(currMode!="Teams" || rivalTeam=="All")
+		teamData = getValues();
+	else
+		teamData = getRivalValues(rivalTeam);
 
 	//now draw
+	if(currMode=="Teams" && rivalTeam=="All")
+		drawTeamData(teamData,0);
+	else if(currMode=="Venues"){
+		drawTeamData(teamData,(wd/10)*1.5);
+		drawHomeTeam(teamData.homeTeam, wd/10,"Home Team:");
+	}
+	else {
+		// drawTeamData({
+		// 	gensetone: [{team: tm, num: 1, i: 0},{team: tmt, num: 2, i: 1}],
+		// 	homesetone: [{team: tm, num: 1, i: 0},{team: tmt, num: 2, i: 1}],
+		// 	oversetone: [{team: tm, num: 1, i: 0},{team: tmt, num: 2, i: 1}],
+		// 	gensettwo: [{team: tm, num: -2, i: 0},{team: tmt, num: -1, i: 1}],
+		// 	homesettwo: [{team: tm, num: -2, i: 0},{team: tmt, num: -1, i: 1}],
+		// 	oversettwo: [{team: tm, num: -2, i: 0},{team: tmt, num: -1, i: 1}],
+		// 	length: 2,
+		// 	teams: [tm,tmt]
+		// },(wd/10)*1.5);
+		drawTeamData(teamData,(wd/teamData.length)*1.5);
+		drawHomeTeam(teams.teamMap[rivalTeam], wd/10,"Rivals Of:");
+	}
+	
+}
+
+function teamRectSettings(s,type,posy,negy,cd,highest){
+   	s.enter()
+   		.append("rect")
+   		.attr("class",type);
+    s.transition()
+    	.duration(aniDur)
+    	.attr("y", function(d){
+        	var i = d.i;
+            if(d.num<0){
+                return cd.mh + negy[i];
+            }
+            else {
+                return cd.mh - cd.bh/highest*d.num - posy[i];
+            }
+        })
+    	.attr("height", function(d){
+        	var i = d.i;
+            var height =  cd.bh/highest*Math.abs(d.num);
+        	if(d.num<0) negy[i] += height; 
+    		else posy[i] += height;
+    		return height;
+        })
+        .attr("x",function(d){
+        	var i = d.i;
+            return cd.x + i*(cd.w/posy.length);
+        })
+        .attr("width", function(d){
+            return (cd.w/posy.length) - cd.bp
+        })
+        .attr("class", function(d){
+        	return type+' '+(d.num>0 ? 'positive' : 'negative');
+        });
+    s.exit().remove();
+}
+
+function drawTeamData(teamData,x){
+	var x = x;
+	var w = wd - x;
 	var barpad = 1;
 	var imgpad = 3;
 	var imgh = h/4
@@ -377,7 +585,19 @@ function redraw(){
 	var bh = (h-imgh)/2
 	var mh = (h-imgh)/2 + imgh;
 
-    var highest = 0;
+	var cd = {
+		bp: barpad,
+		ip: imgpad,
+		ih: imgh,
+		iw: imgw,
+		bh: bh,
+		mh: mh,
+		x: x,
+		w: w
+	}
+	
+
+    var highest = 1;
 
     var posy = Array.apply(null, new Array(teamData.length)).map(Number.prototype.valueOf,0);
     var negy = Array.apply(null, new Array(teamData.length)).map(Number.prototype.valueOf,0);
@@ -392,11 +612,17 @@ function redraw(){
 	genonevals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});	
 	homeonevals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});
 	overonevals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});
+	gentwovals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});	
+	hometwovals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});
+	overtwovals.forEach(function(t){if(t.num>0)posy[t.i]+=t.num; else negy[t.i]+=t.num});
 
     for(var i = 0; i<teamData.length; i++){
     	highest = highest > posy[i] ? highest : posy[i];
     	highest = highest > Math.abs(negy[i]) ? highest : Math.abs(negy[i]);
     }
+
+    svg.selectAll(".home-team")
+    	.remove();
 
     posy = Array.apply(null, new Array(teamData.length)).map(Number.prototype.valueOf,0);
     negy = Array.apply(null, new Array(teamData.length)).map(Number.prototype.valueOf,0);
@@ -406,94 +632,43 @@ function redraw(){
     if(homeonevals.length == 0)
     	homeone.exit().remove();
     else
-    	teamRectSettings(homeone,"home-one",posy,negy,mh,bh,barpad,highest);
+    	teamRectSettings(homeone,"home-one",posy,negy,cd,highest);
 
     var genone = svg.selectAll("rect.gen-one")
     	.data(genonevals, function(d){return d.team.names+".gen-one"});
     if(genonevals.length == 0)
     	genone.exit().remove();
     else
-    	teamRectSettings(genone,"gen-one",posy,negy,mh,bh,barpad,highest);
+    	teamRectSettings(genone,"gen-one",posy,negy,cd,highest);
 
     var overone = svg.selectAll("rect.over-one")
     	.data(overonevals, function(d){return d.team.names+".over-one"});
     if(overonevals.length == 0)
     	overone.exit().remove();
     else
-    	teamRectSettings(overone,"over-one",posy,negy,mh,bh,barpad,highest);
+    	teamRectSettings(overone,"over-one",posy,negy,cd,highest);
 
     var hometwo = svg.selectAll("rect.home-two")
     	.data(hometwovals, function(d){return d.team.names+".home-two"});
     if(hometwovals.length == 0){
-    	console.log("here");
     	hometwo.exit().remove();
     }
     else
-    	teamRectSettings(hometwo,"home-two",posy,negy,mh,bh,barpad,highest);
+    	teamRectSettings(hometwo,"home-two",posy,negy,cd,highest);
 
     var gentwo = svg.selectAll("rect.gen-two")
     	.data(gentwovals, function(d){return d.team.names+".gen-two"});
     if(gentwovals.length == 0)
     	gentwo.exit().remove();
     else
-    	teamRectSettings(gentwo,"gen-two",posy,negy,mh,bh,barpad,highest);
+    	teamRectSettings(gentwo,"gen-two",posy,negy,cd,highest);
 
     var overtwo = svg.selectAll("rect.over-two")
     	.data(overtwovals, function(d){return d.team.names+".over-two"});
     if(overtwovals.length == 0)
     	overtwo.exit().remove();
     else
-    	teamRectSettings(overtwo,"over-two",posy,negy,mh,bh,barpad,highest);
-   		
-
-    // var rects = svg.selectAll("rect")
-    //     .data(teamData, function(d){
-    //     	return d.team.names;
-    //     });
-    // rects.enter()
-    //     .append("rect")
-    //     .attr("x",function(d,i){
-    //         return i*(w/teamData.length);
-    //     })
-    //     .attr("y", function(d){
-    //         if(d.num<0){
-    //             return mh;
-    //         }
-    //         else {
-    //             return Math.floor(mh - bh/highest*d.num);
-    //         }
-    //     })
-    //     .attr("width", function(d,i){
-    //         return (w/teamData.length) - barpad
-    //     })
-    //     .attr("height", function(d){
-    //     	var height = Math.floor(bh/highest*Math.abs(d.num))
-    //         return height;
-    //     })
-    //     .attr("class",function(d){
-    //     	return d.num>0 ? 'positive' : 'negative';
-    //     });
-    // rects.transition()
-    // 	.duration(aniDur).attr("y", function(d){
-    //         if(d.num<0){
-    //             return mh;
-    //         }
-    //         else {
-    //             return Math.floor(mh - bh/highest*d.num);
-    //         }
-    //     })
-    // 	.attr("height", function(d){
-    //         return Math.floor(bh/highest*Math.abs(d.num));
-    //     })
-    //     .attr("x",function(d,i){
-    //         return i*(w/teamData.length);
-    //     })
-    //     .attr("width", function(d,i){
-    //         return (w/teamData.length) - barpad
-    //     })
-    //     .attr("class",function(d){
-    //     	return d.num>0 ? 'positive' : 'negative';
-    //     });
+    	teamRectSettings(overtwo,"over-two",posy,negy,cd,highest);
 
 	//DRAW IMAGES
     var images = svg.selectAll("image")
@@ -503,16 +678,16 @@ function redraw(){
 	images.enter()
 		.append("image")
 		.attr("x",function(d,i){
-            return i*(w/teamData.length);
+            return cd.x + i*(cd.w/teamData.length);
         })
         .attr("y",function(d){
             return 0;
         })
         .attr("width",function(d,i){
-            return imgh;
+            return cd.ih;
         })
         .attr("height",function(d,i){
-            return imgh;
+            return cd.ih;
         })
         .attr("xlink:href", function(d){
         	return 'images/'+d.names+'.png';
@@ -520,44 +695,31 @@ function redraw(){
     images.transition()
     	.duration(aniDur)
         .attr("x",function(d,i){
-            return i*(w/teamData.length);
+            return cd.x + i*(cd.w/teamData.length);
         })
         .attr("width",function(d,i){
-            return imgh;
+            return cd.ih;
         });
+    images.exit().remove();
 }
 
-function teamRectSettings(s,type,posy,negy,mh,bh,barpad,highest){
-   	s.enter()
-   		.append("rect")
-   		.attr("class",type);
-    s.transition()
-    	.duration(aniDur)
-    	.attr("y", function(d){
-        	var i = d.i;
-            if(d.num<0){
-                return mh + negy[i];
-            }
-            else {
-                return mh - bh/highest*d.num - posy[i];
-            }
+function drawHomeTeam(homeTeam,width,text){
+	hometext = svg.append("text");
+	hometext.text(text)
+		.attr("x",width/4)
+		.attr("y",width-5)
+        .attr("class","home-team");
+
+	homeimage = svg.selectAll("image.home-team")
+		.data([homeTeam.names]);
+	homeimage.enter()
+		.append("image")
+		.attr("y",width)
+		.attr("x",width/4)
+        .attr("width",width)
+        .attr("height",width)
+        .attr("xlink:href", function(d){
+        	return 'images/'+d+'.png';
         })
-    	.attr("height", function(d){
-        	var i = d.i;
-            var height =  bh/highest*Math.abs(d.num);
-        	if(d.num<0) negy[i] += height; 
-    		else posy[i] += height;
-    		return height;
-        })
-        .attr("x",function(d){
-        	var i = d.i;
-            return i*(w/posy.length);
-        })
-        .attr("width", function(d){
-            return (w/posy.length) - barpad
-        })
-        .attr("class", function(d){
-        	return type+' '+(d.num>0 ? 'positive' : 'negative');
-        });
-    s.exit().remove();
+        .attr("class","home-team");
 }
